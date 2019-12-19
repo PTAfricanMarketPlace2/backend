0b855f85105def6712893d1cf7ce762be86e4573
kly001 <karenly1961@gmail.com>
2019-12-18 21:49:14 -0500

    setup auth

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
-const erxpress = require('express');
+const express = require('express');
 
 const server = express();
+
+server.use(express.json());
 
 server.get('/',(req,res) => {
     res.status(200).json({api: "Up and running"})
 })
+
+//  Time Stamp:
+//  server.get('/now', (req,res) => {
+//     const now = new Date().toISOString();
+//     res.send(now);
+// })
+module.exports = server;
