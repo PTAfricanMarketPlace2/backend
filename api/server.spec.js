1253f11a6ddb300dd3a1d663a06652af6d6d7ae4
kly001 <karenly1961@gmail.com>
2020-01-09 14:39:18 -0500

    server.spec.js: 2nd & 3rd tests passed

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,7 +2,31 @@ const server = require('./server.js');
 const request = require('supertest');
 
 describe('server.js', () => {
-    test('should be testing environment', () => {
+    it('should be testing environment', () => {
         expect(process.env.DB_ENV).toBe('testing')
     })
+
+describe('GET/', () => {
+    it('should return 200 OK',() => {
+        return request(server)
+        .get('/')
+        .then(res => {
+            expect(res.status).toBe(200);
+        })
+    })
+})
+// Same test using async...await:
+// describe('GET/', () => {
+//     it('should return 200 OK',async() => {
+//         const res = await request(server).get('/')
+//             expect(res.status).toBe(200);
+//     })
+// })
+
+describe('GET/', () => {
+    it('should be json',async() => {
+        const res = await request(server).get('/')
+            expect(res.type).toBe('application/json');
+    })
+})
 })
