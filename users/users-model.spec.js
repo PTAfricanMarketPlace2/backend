bfe6bd5d68b28cf2c33ac2a3c554a2a01c47a2c5
kly001 <karenly1961@gmail.com>
2020-01-09 20:54:41 -0500

    product-model.spec: 1st test working

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -2,10 +2,16 @@ const Users = require('./users-model.js');
 const db = require('../database/dbConfig.js');
 
 describe('users-model', () => {
-    describe('find', () => {
-        it('should find all users in the db', async () => {
+    describe('add', () => {
+        it('should add a user into the db', async () => {
+            await Users.add({username:'user1',password:'password1', firstname:'first', lastname:'name', email:"user1@gmail.com"});
+            await Users.add({username:'user2',password:'password2', firstname:'first', lastname:'name', email:"user2@gmail.com"});
+            
             const users = await db('users')
-            expect(users).toHaveLength(6)
+            expect(users).toHaveLength(2)
+        })
+        beforeEach(async() => {
+            await db('users').truncate()
         })
     })
     
